05e76eb599c4c355f83b2edeb88844929ea36f3d
yhiraki <coffexpr@gmail.com>
2020-05-16 23:04:57 +0900

    extra 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -17,6 +17,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         bold={this.props.lastPutAt === i}
         onClick={() => this.props.onClick(i)}
@@ -25,24 +26,13 @@ class Board extends React.Component {
   }
 
   render() {
+    const a3 = Array(3).fill(null);
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+      a3.fill(null).map((_, i) =>
+        <div className="board-row" key={i}>
+          {a3.fill(null).map((_, j) => this.renderSquare(i * 3 + j))}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      )
     );
   }
 }
